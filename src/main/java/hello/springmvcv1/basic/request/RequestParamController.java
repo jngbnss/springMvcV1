2e1c667e438bfd7fba82ec1c8f0242954af76d8e
jngbn <[user email]>
2025-08-29 18:04:54 +0900

    save 파라미터 필수 여부 - requestParamRequired

---
+++ src/main/java/hello/springmvcv1/basic/request/RequestParamController.java
@@ -0,0 +1,64 @@
+package hello.springmvcv1.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request,
+                               HttpServletResponse response)
+        throws IOException{
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}가 이렇게 해야 로그에서 나오는건가?",username,age);
+
+        response.getWriter().write("request v1");
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username")String memberName,
+                               @RequestParam("age") int memberAge){
+
+        log.info("username={}, age={}가 이렇게 해야 로그에서 나오는건가?",memberName,memberAge);
+
+//        response.getWriter().write("request v1");
+        return "v2 개꿀이네?";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age){
+        log.info("username={}, age={}", username,age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username,int age){
+        log.info("username={}, age={}", username,age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+
+}
