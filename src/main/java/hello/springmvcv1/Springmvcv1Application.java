e4a9233003bd2b178a313850a5d5662ff6600582
jngbn <[user email]>
2025-08-29 15:25:00 +0900

    Initial commit from IntelliJ

---
+++ src/main/java/hello/springmvcv1/Springmvcv1Application.java
@@ -0,0 +1,13 @@
+package hello.springmvcv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvcv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvcv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvcv1/Springmvcv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvcv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvcv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
