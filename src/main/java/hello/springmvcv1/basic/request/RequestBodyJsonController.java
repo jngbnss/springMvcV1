fbe5ac128d8e5b26afead41260fa8179b25e3e61
jngbn <[user email]>
2025-09-01 01:37:22 +0900

    save

---
+++ src/main/java/hello/springmvcv1/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,11 @@
+package hello.springmvcv1.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+    private ObjectMapper objectMapper = new ObjectMapper();
+}
